ab689a048db0b1e355e36b9aec52f2223dfbd242
Ricardo Baumann <ricardo.luis.baumann@gmail.com>
2015-11-03 10:55:41 -0200

    separate containers

--- config/initializers/bunny.rb
+++ config/initializers/bunny.rb
@@ -1,4 +1,4 @@
 #MESSAGING_SERVICE = MessagingService.new(ENV.fetch("AMQP_URL"))
 USE_MESSAGE_QUEUE=false
-#MESSAGING_SERVICE = MessagingService.new if USE_MESSAGE_QUEUE
+MESSAGING_SERVICE = MessagingService.new if USE_MESSAGE_QUEUE
 #MESSAGING_SERVICE.start
