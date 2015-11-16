5be508570b9c37a01bb951bf1e797b833c38d543
Ricardo Baumann <ricardo.luis.baumann@gmail.com>
2015-11-16 15:21:56 -0200

    run containers script

--- run_docker_image.sh
+++ run_docker_image.sh
@@ -8,4 +8,4 @@ docker ps -a | awk '{ print $1,$2 }' | grep mongo | awk '{print $1 }' | xargs -I
 docker run --name some-mongo -d mongo
 docker build -t mesalva/ranking_points .
 docker run -p 3000:3000 -d --name ranking --link some-mongo:mongo  mesalva/ranking_points
-wget http://localhost:3000/aula/1/exercicio/1
+wget -O- http://localhost:3000/aula/1/exercicio/1 
