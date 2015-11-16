7408c4e727e0fd8492a2fa7548563f1cdbda6379
Ricardo Baumann <ricardo.luis.baumann@gmail.com>
2015-11-16 09:43:14 -0200

    mongo testing

--- config/initializers/mongo.rb
+++ config/initializers/mongo.rb
@@ -1,5 +1,4 @@
 require 'rubygems'
 require 'mongo'
-p "\n\n\n\n\n environment here: #{Rails.env}"
 #@settings = YAML.load(File.read(File.join(Rails.root, 'config/mongoid.yml')))[ENV['RAILS_ENV']||'development']
 Mongoid.load!(File.join(Rails.root, 'config/mongoid.yml'))
